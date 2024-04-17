9da2841bf76685826ad0b6ed185b0645bfaf4406
Hai Pham Ngoc <ngochai285nd@gmail.com>
2024-04-17 17:03:10 +0700

    init project

---
+++ src/test/java/com/haiphamcoder/cryptography/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiphamcoder.cryptography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
